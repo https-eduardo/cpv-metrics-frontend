4b48a8a511dc9d63eb9cd6905312a2cfb1e4a53a
Fabio <fabioaraujobruno1313@gmail.com>
2024-09-10 01:14:35 -0300

    Create Card

---
+++ src/components/dashboard/Card.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="flex w-[20vw] h-[140px] text-white bg-[#2d2d2d] rounded-[5px] overflow-hidden">
+  <div class="w-[8px] bg-[#B66CFF]"></div>
+      <div class="flex-col">
+        <div class="flex-grow-1">Texto</div>
+        <div>Valor</div>
+        <div>Icon</div>
+      </div> 
+</div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default{
+  setup(){
+    const list = ref({
+      nome : 'jorge'
+    })
+    return{
+    list
+  };
+}
+}
+</script>
--- src/views/dashboard/Metrics.vue
+++ src/views/dashboard/Metrics.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full bg-[#1C1C1C]">
     <div class="w-full block h-3 bg-[#B66CFF]"></div>
-    <div class="p-8 bg-[#1C1C1C] h-full">
+    <div class="p-8">
       <h1 class="text-2xl font-bold text-white">Métricas</h1>
+    </div >
+
+    <div class="flex justify-evenly">
+      <DashboardCard/>
+      <DashboardCard/>
+      <DashboardCard/>
+      <DashboardCard/>
     </div>
+
   </div>
 </template>
+
+<script setup lang="ts">
+  import DashboardCard from "../../components/dashboard/Card.vue";
+</script>
