194738d775e78bccefade16f30a07c24ec79c081
Fabio <fabioaraujobruno1313@gmail.com>
2024-09-10 22:49:02 -0300

    change width and height

--- src/components/dashboard/Card.vue
+++ src/components/dashboard/Card.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex w-1/5 h-32 text-white bg-[#2d2d2d] rounded-md overflow-hidden">
+  <div class="flex w-72 h-32 text-white bg-[#2d2d2d] rounded-md overflow-hidden">
     <div class="w-2 bg-[#B66CFF]"></div>
     <div class="flex-1 flex flex-col">
-      <div class="flex items-center h-1/3 px-3">
-        <div>{{ title }}</div>
+      <div class="flex items-center h-12 px-3">
+        <div>{{title}}</div>
       </div>
-      <div class="flex justify-between items-end h-2/5 px-2">
+      <div class="flex justify-between items-end h-12 px-2">
         <div class="text-2xl">{{value}}</div>
         <component :is="icon"></component>
       </div>
--- src/views/dashboard/Metrics.vue
+++ src/views/dashboard/Metrics.vue
@@ -3,7 +3,7 @@
     <div class="w-full block h-3 bg-[#B66CFF]"></div>
     <div class="p-8">
       <h1 class="text-2xl font-bold text-white">Métricas</h1>
-    </div >
+    </div >  
   </div>
 </template>
 
